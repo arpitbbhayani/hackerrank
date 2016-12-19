a13859c0dc71e89b4ecd0987b4185cc2f3e8ca49
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-12-19 07:59:10 +0530

    Updating pat w.r.t new machine.

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ git add $4
 git commit -m "Adding solution to $1"
 git push origin master
 
-cd ~/arpitbhayani.me/
+cd ~/workspace/arpitbhayani.me/
 python scripts/add-new-programming-question.py hackerrank "$1" "$2" "$3" $4
 git add --all
 git commit -m "Adding solution of $2"
